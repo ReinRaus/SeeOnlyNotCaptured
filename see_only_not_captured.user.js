409e6f9817b7e7b93f8faaa765c76540ac68c866
ReinRaus <reinraus.nax@gmail.com>
2017-07-04 23:06:16 +0300

    v1.0.6
    - Облом с поддержкой GreaseMonkey, откат как было

--- see_only_not_captured.user.js
+++ see_only_not_captured.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         See only not captured portals
 // @namespace    https://upor.in/caps/
-// @version      1.0.5
+// @version      1.0.6
 // @description  Now you see me
 // @author       ReinRaus
 // @updateURL    https://github.com/ReinRaus/SeeOnlyNotCaptured/raw/master/see_only_not_captured.user.js
