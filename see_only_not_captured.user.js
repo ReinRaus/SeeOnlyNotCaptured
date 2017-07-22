e9ed5369585b38f67e416bbd714926fcbe4b700b
ReinRaus <reinraus.nax@gmail.com>
2017-07-22 20:52:10 +0300

    fix

--- see_only_not_captured.user.js
+++ see_only_not_captured.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         See only not captured portals
 // @namespace    https://upor.in/caps/
-// @version      1.2.6.1
+// @version      1.2.7
 // @description  Now you see me
 // @author       ReinRaus
 // @updateURL    https://github.com/ReinRaus/SeeOnlyNotCaptured/raw/master/see_only_not_captured.user.js
