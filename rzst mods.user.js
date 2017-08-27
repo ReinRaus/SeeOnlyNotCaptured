6060798d90a022e158812a36bd2ce78bbcf0f31a
ReinRaus <reinraus.nax@gmail.com>
2017-08-27 19:17:23 +0300

    v 1.0.2 Показывает даты медалей для последнего захвата

--- rzst mods.user.js
+++ rzst mods.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         rzst mods
 // @namespace    https://rzst.io
-// @version      1.0.1
+// @version      1.0.2
 // @description  Now you see me
 // @author       ReinRaus
 // @updateURL    https://github.com/ReinRaus/SeeOnlyNotCaptured/raw/master/rzst_mods.user.js
@@ -47,5 +47,15 @@ $("table td[valign='top'] table").each( (index,table)=> {
         map_time_td[ Date.parse( td.innerHTML.match( /\slast:\s([^]+?)\s</i )[1] ) ] = td;
     } );
     var sortedKeys = Object.keys( map_time_td ).map( n=>parseInt(n) ).sort().reverse();
+    if ( index === 0 ) {
+        var medals = { gold:20, platin: 90, onyx: 150 };
+        var tdLastCapture = map_time_td[ sortedKeys[0] ];
+        var text = "<small><pre>";
+        for ( var medal in medals ) {
+            text+= `${medal}&#9;`+(new Date( sortedKeys[0]+medals[medal]*1000*60*60*24 )).toJSON().split("T")[0].split("-").reverse().join(".")+"\n";
+        }
+        map_time_td[ sortedKeys[0] ].innerHTML += text;
+    }
+    console.log( sortedKeys[0] );
     sortedKeys.forEach( key=>$( map_time_td[key] ).parent().insertAfter( $(table).find( "tr:last") ));
 } );
